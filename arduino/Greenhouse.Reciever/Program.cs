a356fb61d2f69533afe8e103e12753a98d6df79e
Gaute Brakstad <gbr@dips.no>
2021-09-23 11:27:51 +0200

    Program that reads ouput from arduino

---
+++ arduino/Greenhouse.Reciever/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Greenhouse.Reciever
+{
+    class Program
+    {
+        static SerialPort sp = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
+        private static string outputfile = @"c:\temp\greenhouse\test.json";
+
+        static void Main(string[] args)
+        {
+            sp.DataReceived += new SerialDataReceivedEventHandler(Sp_DataReceived);
+            sp.Open();
+            Console.Read();
+
+        }
+        private static void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            List<Metric> metrics = new List<Metric>();
+            var value = sp.ReadLine();
+            var metricsArray = value.Split(';');
+            if (metricsArray.Length == 0)
+            {
+                return;
+            }
+            foreach(var metric in metricsArray)
+            {
+                var metricInformation = metric.Split(' ');
+                if(metricInformation.Length != 3)
+                {
+                    continue;
+                }
+                metrics.Add(new Metric { Timestamp =DateTime.Now, Name= metricInformation[0], Value=metricInformation[1], Unit=metricInformation[2] });
+            }
+            if (metrics.Count > 0) 
+            { 
+                string jsonString = JsonSerializer.Serialize(metrics);
+
+                if (!File.Exists(outputfile))
+                {
+                    using (StreamWriter sw = File.CreateText(outputfile))
+                    {
+                        sw.WriteLine(jsonString);
+                    }
+                }
+                using (StreamWriter sw = File.AppendText(outputfile))
+                {
+                    sw.WriteLine(jsonString);
+                }
+            }
+        }
+    }
+}
--- arduino/greenhouse_metrics/greenhouse_metrics.ino
+++ arduino/greenhouse_metrics/greenhouse_metrics.ino
@@ -2,26 +2,31 @@
 #define dht_apin A0 // Analog Pin sensor is connected to
  
 dht DHT;
- 
+int val;
+int ledpin=13; 
+
 void setup(){
  
   Serial.begin(9600);
   delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  pinMode(ledpin,OUTPUT);  
   delay(1000);//Wait before accessing Sensor
  
 }
  
 void loop(){
- 
+
+   
     DHT.read11(dht_apin);
-    
-    Serial.print("Current humidity = ");
+    Serial.print(";");
+    Serial.print("Humidity ");
     Serial.print(DHT.humidity);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
+    Serial.print(" %");
+    Serial.print(";");
+    Serial.print("Temperature-Air ");
     Serial.print(DHT.temperature); 
-    Serial.println("C  ");
+    Serial.print(" C");
+    Serial.println(";");
     
     delay(5000);//Wait 5 seconds before accessing sensor again.
  
